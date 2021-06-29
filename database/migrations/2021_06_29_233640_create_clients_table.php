90a3d40b7a63c82032b0898a5925f3a5aa2dbf67
Oussama <oussamahtx@gmail.com>
2021-06-30 00:45:06 +0100

    'ArmadilloV0.1'

---
+++ database/migrations/2021_06_29_233640_create_clients_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->string('prenom');
+            $table->string('email');
+            $table->string('tel');
+            $table->string('ville');
+            $table->date('date_naiss');
+            $table->string('adress');
+            $table->string('rib');
+            $table->string('signature');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layout.index');
+});
+Route::get('/admin-dashboard', function () {
+    return view('admin.admin-dashboard');
 });
